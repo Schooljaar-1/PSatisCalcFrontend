1d963cb196ae1b0ad1ed31c90405edb96706cf29
SAAB.W <woutersaab@gmail.com>
2025-07-23 19:08:05 +0200

    Styling better responsive now for 1080 and 1440. Also added last container where flowchart will sit inside of main page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import Topbar from './Topbar.jsx'
 import Description  from './Description.jsx'
 import Recipes from './Recipes.jsx'
+import Flowchart from './Flowschart.jsx'
+import './Styling/app.css'
 
 function App() {
     return (
     <>
-      <div className='page'>
-        <Topbar />
-        <Description />
-        <Recipes />
+      <div className='mainPage'>
+        <div className='header'>
+          <Topbar />
+        </div>
+        <div className='main'>
+          <div className='sidebar'>
+            <Description />
+            <Recipes />
+          </div>
+          <div className='content'>
+            <Flowchart />
+          </div>
+        </div>
       </div>
     </>
   )
---
+++ src/Flowschart.jsx
@@ -0,0 +1,14 @@
+import './Styling/global.css'
+import './Styling/flowchart.css'
+
+function Flowchart(){
+    return (
+        <>
+        <div className='flowBox'>
+            <p>hi</p>
+        </div>
+        </>
+    )
+};
+
+export default Flowchart
