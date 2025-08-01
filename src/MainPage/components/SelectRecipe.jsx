979436bd0ef3d374996433659639281b7ea581a3
SAAB.W <woutersaab@gmail.com>
2025-08-01 17:52:12 +0200

    Added images, better div management for mapped data. Loading spinner icon added.

--- src/MainPage/components/SelectRecipe.jsx
+++ src/MainPage/components/SelectRecipe.jsx
@@ -7,7 +7,7 @@ function SelectRecipe(){
     // Setting URL for fetch
     const API_URL = import.meta.env.VITE_API_URL;
 
-    const [recipeData, setRecipeData] = useState(mockData);
+    const [recipeData, setRecipeData] = useState(null);
     const [userInput, setUserInput] = useState(null);
     
     // ---------FETCHING DATA FROM BACKEND-----------
@@ -35,25 +35,49 @@ function SelectRecipe(){
     }
     // ---------FETCHING DATA FROM BACKEND-----------
 
-    let content = mockData.map(object => (
-        <div className='objectMappingTest' key={object.name}>
-            {object.name}
+    // let content = mockData.map(object => (
+    //     <div className='objectMappingTest' key={object.name}>
+    //         {object.name}
+    //     </div>
+    // ));
+    let content;
+
+    if (recipeData === null) {
+    content = (
+        <div className="spinner"></div>
+    );
+    } 
+    else {
+    content = recipeData.map(object => (
+        <div className='objectMapping'>
+            <div className='objectImageDiv'>
+                <img 
+                    className='objectImage'
+                    key={`${object.name.replace(/\s+/g, '_')}_image`}
+                    src={`/recipeImages/${object.name.replace(/\s+/g, '_')}_image.png`}
+                />
+                {console.log(`${object.name.replace(/\s+/g, '_')}_image.png`)}       
+
+            </div>
+            <div>
+                Pluis
+            </div>
         </div>
     ));
-
-    // if (recipeData === null) {
-    // content = <p><i>Loading...</i></p>;
-    // } else {
-    // content = <p>worked</p>;
-    // }
+    }
+    
     
     return (
         <>
         <div className='selectRecipeContainer'>
             <input className='userInputField' type="text" placeholder='Enter a recipe name...' onChange={handleUserInput} id="recipe-name"/>
-            {/* TODO: make the loading show a loading icon instead of silly text. */}
+            
+            {/* TODO: Make up divs, also code to show unknown image if the image isn't added to the public folder yet */}
             {/* TODO: unpack the data and make them into nice divs with buttons to add them to the calculation */}
-            {content}
+
+            <div className='generatedDivList'>
+                {content}
+            </div>
         </div>
         </>
     )
