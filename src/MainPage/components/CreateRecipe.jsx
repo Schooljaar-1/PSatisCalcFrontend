e48bd2a66814ebca234c690ae0e09943d10ef8b8
BaasW <wan.baas@student.avans.nl>
2025-09-02 21:08:49 +0200

    Create recipe useState now consists of object eventually sent to API

--- src/MainPage/components/CreateRecipe.jsx
+++ src/MainPage/components/CreateRecipe.jsx
@@ -7,21 +7,36 @@ function CreateRecipe(){
     // Setting URL for fetch
     const API_URL = import.meta.env.VITE_API_URL;
 
-    // Replace item image useState variable with the whole object that will eventually be sent over. 
-    const [itemImageName, setQuery] = useState("");
+    const [recipe, setRecipe] = useState(
+        {
+            name: "",
+            version: "",
+            machine: "",
+            amount: 0,
+            type: "",
+            image: "",
+            parts: []
+        }
+    );
 
-    const HandleUserInput = ( {target} ) => {
-        setQuery(target.value)
-    }
+    const HandleUserInput = ({ target }) => {
+        setRecipe(prev => ({
+            ...prev,
+            image: target.value
+        }));
+    };
 
     // Generating search window items
     let content;
     content = recipeImageNames
-        .filter(object => itemImageName === null || object.replace(/\s+/g, '').toLowerCase().includes(itemImageName.replace(/\s+/g, '').toLowerCase()))
+        .filter(object => !recipe.image || object.replace(/\s+/g, '').toLowerCase().includes(recipe.image.replace(/\s+/g, '').toLowerCase()))
         .map(object => {
             const imageName = object.replace(/\s+/g, '');
             return (
-                <div className='createObjectMapping' key={object} onClick={()=>setQuery(imageName)}>
+                <div className='createObjectMapping' key={object} onClick={() => setRecipe(prev => ({
+                    ...prev,
+                    image: imageName
+                }))}>
                     <img 
                         className='queryItemImage'
                         src={`recipeImages/${imageName}.png`}
@@ -42,8 +57,8 @@ function CreateRecipe(){
                     <div className='createRecipeSelectImage'>
                         <img 
                             className='itemImage'
-                            src={`/recipeImages/${itemImageName}.png`}
-                            alt={`${itemImageName}.png`}
+                            src={`/recipeImages/${recipe.image}.png`}
+                            alt={`${recipe.image}.png`}
                             onError={(e) => {
                                 e.target.onerror = null;
                                 e.target.src = '/recipeImages/unknown.png';
@@ -55,7 +70,7 @@ function CreateRecipe(){
                             placeholder='Enter name of item...' 
                             onChange={HandleUserInput} 
                             id="recipe-name" 
-                            value={itemImageName}/>
+                            value={recipe.image}/>
                     </div>
                     <div className='createRecipeSelectWindow'>
                         {content}
