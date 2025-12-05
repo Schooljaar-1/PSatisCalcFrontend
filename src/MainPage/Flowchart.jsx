59bf48b6ead2402e105a1c829d843e1f72bf5cb9
BaasW <wan.baas@student.avans.nl>
2025-12-05 19:07:25 +0100

    All inputs gathered inside flowchart parts list. Now to add logic to them.

--- src/MainPage/Flowchart.jsx
+++ src/MainPage/Flowchart.jsx
@@ -2,23 +2,64 @@ import '../MainPage_Styling/global.css'
 import '../MainPage_Styling/flowchart.css'
 
 function Flowchart({ recipes }){
+
+    // Mapping to add selected parts into the list on the left of flowchart area
+    let selectedParts;
+    selectedParts = recipes.map(part => {
+        return(
+            <div className='singleSelectedPart' key={`${part.name} ${part.version}`}>
+                <div className='selectPartTopHalf'>
+                    <div className='partImageDiv' onClick={() => addRecipeToSelected(object)}>
+                        <img 
+                            className='partImage'
+                            src={`/recipeImages/${part.image}.png`}
+                            alt={part.name}
+                            onError={(e) => {
+                                e.target.onerror = null;
+                                e.target.src = '/recipeImages/unknown.png';
+                            }}
+                        />
+                    </div>
+                    <div className='partDescriptionDiv'>
+                        <div className='partBasicDescription'>
+                            <p><b>Name:</b> {part.name}</p>
+                            <p><b>Machine:</b> {part.machine}</p>
+                            <p><b>Parts:</b> {part.parts.map(part => `${part.partName}(${part.amount.teller}/${part.amount.noemer})`).join(', ')}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='selectedPartBottomHalf'>
+                    <b>Amount: </b>
+                    <input 
+                        className='createRecipePartAmountInput' 
+                        type="number" 
+                        placeholder='T'
+                    />
+                    <p>/ </p>
+                    <input 
+                        className='createRecipePartAmountInput' 
+                        type="number" 
+                        placeholder='N'
+                    />
+                    <p>Per minute</p>
+                </div>
+            </div>
+            );
+        });
+
+
     return (
         <>
         <div className='flowBox'>
-            <div className='SelectedRecipesList'>
+            <div className='selectedRecipesList'>
                 <h1>Selected Recipes</h1>
-                <div style={{ borderBottom: '2px solid black', width: '100%', margin: '0.2em 0 1em 0' }}></div>
+                <div style={{ borderBottom: '2px solid white', width: '100%', margin: '0.2em 0 1em 0' }}></div>
+                <div className='selectedPartsList'>
+                    {selectedParts}
+                </div>
+                <div className='selectedPartsActionButtons'>
 
-                {/* TODO: Map the incoming recipes with their image, name and version. Then also add a field where desired amount per min can be specified */}
-                {recipes && recipes.length > 0 ? (
-                    recipes.map((recipe) => (
-                        <p key={recipe.name}>{recipe.name}</p> // show recipe names
-                    ))
-                ) : (
-                    <p>No recipes selected</p>
-                )}
-
-
+                </div>
             </div>
             <div className='FlowchartArea'>
 
@@ -29,3 +70,13 @@ function Flowchart({ recipes }){
 };
 
 export default Flowchart
+
+
+{/* TODO: Map the incoming recipes with their image, name and version. Then also add a field where desired amount per min can be specified
+{recipes && recipes.length > 0 ? (
+    recipes.map((recipe) => (
+        <p key={recipe.name}>{recipe.name}</p> // show recipe names
+    ))
+) : (
+    <p>No recipes selected</p>
+)} */}
